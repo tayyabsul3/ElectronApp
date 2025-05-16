87a3cd10f1ca67a7de7e358c6ba5c15a76ec3f4b
Muhammad Tayyab <tayyabsultan621@gmail.com>
2025-05-16 21:31:55 +0500

    latest code

---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge, ipcRenderer } = require("electron");
+console.log("preload loaded");
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke("ping"),
+});
